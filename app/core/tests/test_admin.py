3dc3e66cc352cfc3472bdd8064f32d5434bed0a5
Kira-UIT <thiennguyen.fsd@gmail.com>
2022-08-16 00:09:35 +0700

    fix test admin

--- app/core/tests/test_admin.py
+++ app/core/tests/test_admin.py
@@ -23,7 +23,7 @@ class AdminSiteTests(TestCase):
             name='testuser'
         )
 
-    def test_user_list(self):
+    def test_users_lists(self):
         """Test list user are listed on page"""
         url = reverse('admin:core_user_changelist')
         res = self.client.get(url)
